a198103ec5fde472987f2d2ebd51646b42a96c96
szhao23 <stevenzhao456@gmai.com>
2023-03-21 06:25:29 -0500

    Finished JavaScript code functions to implement Show/Hide Password as well as preventDefault to prevent the Form from Submitting

---
+++ code/index.js
@@ -0,0 +1,29 @@
+// Select the Form Element & Store it in a variable
+const forms = document.querySelector(".forms"),
+  pwShowHide = document.querySelectorAll(".eye-icon"),
+  links = document.querySelectorAll(".link");
+
+// Loop for Show/Hide Password
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    let pwFields =
+      eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwFields.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // prevents form from submitting
+    forms.classList.toggle("show-signup");
+  });
+});
